ca1d4f8a24ffb9f6f409a031f8f0c5242cba6fd4
Rasty Espartero <gemrasty@gmail.com>
2023-08-27 23:29:05 +0800

    Add files via upload

---
+++ Practice Exercises/Prime Number Generator.py
@@ -0,0 +1,18 @@
+def is_prime(n):
+    if n <= 1:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+def prime_generator():
+    num = 2
+    while True:
+        if is_prime(num):
+            yield num
+        num += 1
+
+prime_gen = prime_generator()
+for _ in range(10):
+    print(next(prime_gen))
