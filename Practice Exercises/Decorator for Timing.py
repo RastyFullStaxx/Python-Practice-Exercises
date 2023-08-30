9a1ee737d702daea7e024c41ac98a194ee0d1090
Rasty Espartero <gemrasty@gmail.com>
2023-08-30 22:51:11 +0800

    Add files via upload

---
+++ Practice Exercises/Decorator for Timing.py
@@ -0,0 +1,17 @@
+import time
+
+def timing_decorator(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        execution_time = end_time - start_time
+        print(f"Execution time: {execution_time:.4f} seconds")
+        return result
+    return wrapper
+
+@timing_decorator
+def slow_function():
+    time.sleep(2)
+
+slow_function()
