b7c4b09b8853303b88e45d892e9c4377db49eb04
Rasty Espartero <gemrasty@gmail.com>
2023-08-31 11:58:29 +0800

    Add files via upload

---
+++ Practice Exercises/Recursive Power.py
@@ -0,0 +1,9 @@
+def power(base, exponent):
+    if exponent == 0:
+        return 1
+    return base * power(base, exponent - 1)
+
+base = 2
+exponent = 5
+result = power(base, exponent)
+print(result)
