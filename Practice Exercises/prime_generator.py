9a6c6475006554d9fcc8e36357651e375cb25e85
Rasty Espartero <gemrasty@gmail.com>
2023-09-06 23:19:42 +0800

    Create prime_generator.py

---
+++ Practice Exercises/prime_generator.py
@@ -0,0 +1,12 @@
+def prime_generator():
+    primes = []
+    num = 2
+    while True:
+        if all(num % prime != 0 for prime in primes):
+            yield num
+            primes.append(num)
+        num += 1
+
+prime_gen = prime_generator()
+for _ in range(10):
+    print(next(prime_gen))
