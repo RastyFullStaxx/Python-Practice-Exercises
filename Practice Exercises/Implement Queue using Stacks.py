09751844271c111429107b34cb36b37508efe574
Rasty Espartero <gemrasty@gmail.com>
2023-09-10 23:37:26 +0800

    Create Implement Queue using Stacks.py

---
+++ Practice Exercises/Implement Queue using Stacks.py
@@ -0,0 +1,16 @@
+class MyQueue:
+
+    def __init__(self):
+        self.stack1 = []
+        self.stack2 = []
+
+    def push(self, x: int) -> None:
+        self.stack1.append(x)
+
+    def pop(self) -> int:
+        self.peek()
+        return self.stack2.pop()
+
+    def peek(self) -> int:
+        if not self.stack2:
+            while self.stack
