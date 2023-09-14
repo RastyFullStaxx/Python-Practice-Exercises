b2536949b6da117c24d9e5c1bb2b9a1e88835f1f
Rasty Espartero <gemrasty@gmail.com>
2023-09-14 22:56:30 +0800

    Create Reverse Vowels of a String.py

---
+++ Practice Exercises/Reverse Vowels of a String.py
@@ -0,0 +1,19 @@
+def reverse_vowels(s):
+    vowels = "aeiouAEIOU"
+    s = list(s)
+    left, right = 0, len(s) - 1
+
+    while left < right:
+        while left < right and s[left] not in vowels:
+            left += 1
+        while left < right and s[right] not in vowels:
+            right -= 1
+        s[left], s[right] = s[right], s[left]
+        left += 1
+        right -= 1
+
+    return ''.join(s)
+
+text = "hello"
+result = reverse_vowels(text)
+print(result)
