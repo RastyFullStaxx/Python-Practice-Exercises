1714d02cc7f6cb08c704ce48e5a1e3a9b1109750
Rasty Espartero <gemrasty@gmail.com>
2023-09-14 22:57:23 +0800

    Create First Missing Positive.py

---
+++ Practice Exercises/First Missing Positive.py
@@ -0,0 +1,16 @@
+def first_missing_positive(nums):
+    n = len(nums)
+
+    for i in range(n):
+        while 1 <= nums[i] <= n and nums[nums[i] - 1] != nums[i]:
+            nums[nums[i] - 1], nums[i] = nums[i], nums[nums[i] - 1]
+
+    for i in range(n):
+        if nums[i] != i + 1:
+            return i + 1
+
+    return n + 1
+
+array = [3, 4, -1, 1]
+result = first_missing_positive(array)
+print(result)
