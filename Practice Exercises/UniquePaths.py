34c87bdf009c66b81a4a1fd02c57371035e90c19
Rasty Espartero <gemrasty@gmail.com>
2023-09-22 23:31:46 +0800

    Create UniquePaths.py

---
+++ Practice Exercises/UniquePaths.py
@@ -0,0 +1,12 @@
+def unique_paths(m, n):
+    dp = [[1] * n for _ in range(m)]
+
+    for i in range(1, m):
+        for j in range(1, n):
+            dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+
+    return dp[-1][-1]
+
+m_value = 3
+n_value = 7
+result = unique_paths(m_value, n_value)
