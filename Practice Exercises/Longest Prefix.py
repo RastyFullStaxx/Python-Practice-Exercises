b42637e7b58021ee1a5bbebe01aa0b3abfd9a9a9
Rasty Espartero <gemrasty@gmail.com>
2023-09-24 23:45:52 +0800

    Create Longest Prefix.py

---
+++ Practice Exercises/Longest Prefix.py
@@ -0,0 +1,19 @@
+def longest_common_prefix(strs):
+    if not strs:
+        return ""
+
+    min_len = min(len(s) for s in strs)
+    prefix = ""
+
+    for i in range(min_len):
+        char = strs[0][i]
+        if all(s[i] == char for s in strs):
+            prefix += char
+        else:
+            break
+
+    return prefix
+
+# Example usage
+strings = ["flower", "flow", "flight"]
+result = longest_common_prefix(strings)
