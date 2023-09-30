b9530af700181694a00b92cfffce6a7d3bbde4ed
Rasty Espartero <gemrasty@gmail.com>
2023-09-30 22:52:05 +0800

    Create Dec.py

---
+++ Practice Exercises/Dec.py
@@ -0,0 +1,15 @@
+import time
+
+def measure_time(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        print(f"{func.__name__} took {end_time - start_time} seconds to execute.")
+        return result
+    return wrapper
+
+@measure_time
+def some_function():
+    # Your code here
+    pass
