2dde3134a77a3fe3a7f3057ed60205f7a7a3a275
Rasty Espartero <gemrasty@gmail.com>
2023-10-16 23:42:29 +0800

    Create Decorators II.py

---
+++ Practice Exercises/Decorators II.py
@@ -0,0 +1,16 @@
+# Implement a decorator that measures and prints the execution time of a function.
+import time
+
+def measure_time(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        print(f"{func.__name__} took {end_time - start_time} seconds to execute.")
+        return result
+    return wrapper
+
+@measure_time
+def some_function():
+    # Your code here
+    pass
