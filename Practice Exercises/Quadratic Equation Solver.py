fe0818184819c929185bb93798d7b6f0a122fb71
Rasty Espartero <gemrasty@gmail.com>
2023-10-27 23:41:02 +0800

    Create Quadratic Equation Solver.py

---
+++ Practice Exercises/Quadratic Equation Solver.py
@@ -0,0 +1,20 @@
+# Implement a quadratic equation solver.
+import math
+
+def solve_quadratic(a, b, c):
+    discriminant = b**2 - 4*a*c
+    if discriminant > 0:
+        root1 = (-b + math.sqrt(discriminant)) / (2*a)
+        root2 = (-b - math.sqrt(discriminant)) / (2*a)
+        return root1, root2
+    elif discriminant == 0:
+        root = -b / (2*a)
+        return root
+    else:
+        return "No real roots"
+
+a = 1
+b = -3
+c = 2
+roots = solve_quadratic(a, b, c)
+print(roots)
