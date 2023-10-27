52c84d103163e7d2392649ed1e2d45948efa78e1
Rasty Espartero <gemrasty@gmail.com>
2023-10-27 23:40:06 +0800

    Create Reversed String.py

---
+++ Practice Exercises/Reversed String.py
@@ -0,0 +1,7 @@
+# Implement a function to reverse a string.
+def reverse_string(input_string):
+    return input_string[::-1]
+
+text = "Hello, world!"
+reversed_text = reverse_string(text)
+print(reversed_text)
