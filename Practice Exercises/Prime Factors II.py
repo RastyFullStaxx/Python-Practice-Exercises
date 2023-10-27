47794bb883823bf6cf4fe237c90b1c99f50dc4a8
Rasty Espartero <gemrasty@gmail.com>
2023-10-27 23:40:48 +0800

    Create Prime Factors II.py

---
+++ Practice Exercises/Prime Factors II.py
@@ -0,0 +1,14 @@
+# Write a function that finds the prime factors of a number.
+def prime_factors(n):
+    factors = []
+    divisor = 2
+    while n > 1:
+        while n % divisor == 0:
+            factors.append(divisor)
+            n //= divisor
+        divisor += 1
+    return factors
+
+number = 56
+factors = prime_factors(number)
+print(f"Prime factors of {number}: {factors}")
