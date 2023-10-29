783f6fe00c06e801cb0cad015efd8c8a8b9c6ac8
Rasty Espartero <gemrasty@gmail.com>
2023-10-29 23:24:40 +0800

    Create List Intersection II.py

---
+++ Practice Exercises/List Intersection II.py
@@ -0,0 +1,8 @@
+# Write a function to find the intersection of two lists.
+def list_intersection(list1, list2):
+    return list(set(list1) & set(list2)
+
+list_a = [1, 2, 3, 4, 5]
+list_b = [3, 4, 5, 6, 7]
+intersection = list_intersection(list_a, list_b)
+print(intersection)
