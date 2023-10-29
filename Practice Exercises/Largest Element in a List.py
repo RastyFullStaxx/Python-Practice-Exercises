fe56611ba88b08a5bb4bdef96fba5a75f5d7f69f
Rasty Espartero <gemrasty@gmail.com>
2023-10-29 23:23:31 +0800

    Create Largest Element in a List.py

---
+++ Practice Exercises/Largest Element in a List.py
@@ -0,0 +1,7 @@
+# Write a function to find the largest element in a list.
+def find_largest_element(lst):
+    return max(lst)
+
+numbers = [7, 13, 2, 10, 6, 8]
+largest = find_largest_element(numbers)
+print(largest)
