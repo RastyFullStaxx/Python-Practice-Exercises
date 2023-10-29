fb9919ae17f15424073d72b5bf91e7012ae7a32a
Rasty Espartero <gemrasty@gmail.com>
2023-10-29 23:21:42 +0800

    Create Factorial Calculation.py

---
+++ Practice Exercises/Factorial Calculation.py
@@ -0,0 +1,9 @@
+# Write a function to calculate the factorial of a given number.
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+result = factorial(5)
+print(result)
